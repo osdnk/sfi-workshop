cc5ce14a2df9b8d240ac9bfef363e54c846b05be
osdnk <micosa97@gmail.com>
2018-03-31 17:14:56 +0200

    Remove useless imports

--- extra-task/App.js
+++ extra-task/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Wrapper from './src/components/Wrapper';
 import firebase from 'firebase';
 import { config } from './config';
